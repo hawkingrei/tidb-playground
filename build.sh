b6b8ab3b57f8c1a4f11851ac4ad35503af9f5c5b
Weizhen Wang <hawking.rei@gmail.com>
2025-07-28 11:30:34 +0800

    update meta
    Signed-off-by: Weizhen Wang <hawking.rei@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -x
-TIDB_VERSION_SLICE=('v5.1.1' 'v5.1.0' 'v5.0.3' 'v5.2.0' 'v5.3.0')
+TIDB_VERSION_SLICE=('v7.5.6' 'v8.1.2' 'v8.5.2' 'nightly')
 
 for version in "${TIDB_VERSION_SLICE[@]}"
 do
     export TIDB_VERSION=${version}
     echo ${version}
-	docker build -t hooopo/tidb-playground:${TIDB_VERSION}  --build-arg TIDB_VERSION .
-    docker push hooopo/tidb-playground:${TIDB_VERSION}
+	docker build -t hawkingrei/tidb-playground:${TIDB_VERSION}  --build-arg TIDB_VERSION .
+    docker push hawkingrei/tidb-playground:${TIDB_VERSION}
 done
