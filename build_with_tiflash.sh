e1227b0d3972b328afb8edbfb28bc91d128b5adc
Weizhen Wang <wangweizhen@pingcap.com>
2025-12-06 18:23:10 +0800

    chore: add dependabot
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- build_with_tiflash.sh
+++ build_with_tiflash.sh
@@ -3,6 +3,21 @@ set -x
 current_date=$(date +%Y-%m-%d)
 TIDB_VERSION_SLICE=('v7.5.7' 'v8.1.2' 'v8.5.3' "nightly")
 
+NO_PUSH=false
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --no-push)
+            NO_PUSH=true
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
 for version in "${TIDB_VERSION_SLICE[@]}"
 do
 
@@ -12,7 +27,11 @@ do
     fi
     echo ${version}
     docker build -t hawkingrei/tidb-playground:${TIDB_VERSION}-with-tiflash --no-cache  --build-arg TIDB_VERSION=${version} -f Dockerfile_with_tiflash .
-    docker push hawkingrei/tidb-playground:${TIDB_VERSION}-with-tiflash
+    if [ "$NO_PUSH" = false ]; then
+        docker push hawkingrei/tidb-playground:${TIDB_VERSION}-with-tiflash
+    fi
 done
 docker tag hawkingrei/tidb-playground:nightly-${current_date}-with-tiflash  hawkingrei/tidb-playground:nightly-with-tiflash
-docker push hawkingrei/tidb-playground:nightly-with-tiflash
+if [ "$NO_PUSH" = false ]; then
+    docker push hawkingrei/tidb-playground:nightly-with-tiflash
+fi
